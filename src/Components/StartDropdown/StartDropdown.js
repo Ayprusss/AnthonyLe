ca971ac62309fa275a028828937e64485b7bb72f
theant741@gmail.com <theant741@gmail.com>
2024-09-13 11:35:07 -0400

    added StartDropdown component

---
+++ src/Components/StartDropdown/StartDropdown.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./StartDropdown.css";
+
+
+function StartDropdown() {
+    <div className="start-dropdown-div">
+        <div className="start-dropdown window">
+            <div className="title-bar">
+                <div className=" title-bar-text">Windows 98: Anthony Le</div>
+            </div>
+            <div className="windows-body">
+                <p>Check out my the apps below!</p>
+            </div>
+        </div>
+    </div>
+}
+
+
+export default StartDropdown;
