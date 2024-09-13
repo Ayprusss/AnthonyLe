4ce69ccb85a963bc2a7e503302a546c6a42bb063
theant741@gmail.com <theant741@gmail.com>
2024-09-13 11:35:46 -0400

    added changes to homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './Home.css';
 import Taskbar from "../../Components/TaskBar/Taskbar.js";
-
+import PhotoWindow from "../../Components/PhotoWindow/PhotoWindow.js";
+import TextWindow from "../../Components/TextWindow/TextWindow.js";
 function Home() {
     return(
         <div>
             <div className="home">
+                <PhotoWindow />
+                <TextWindow />
                 <Taskbar />
             </div>
         </div>
