39d759cf31f0079a1590573ca446126ae0b537ab
theant741@gmail.com <theant741@gmail.com>
2024-09-13 11:34:22 -0400

    created PhotoWindow page

--- src/Components/PhotoWindow/PhotoWindow.js
+++ src/Components/PhotoWindow/PhotoWindow.js
@@ -2,16 +2,33 @@ import React from "react";
 import "98.css";
 import "./PhotoWindow.css";
 
+import "98.css"
+import PortfolioPhoto1 from "./portfolioPhoto.jpg"
+
+import { Rnd } from "react-rnd";
 function photoWindow() {
+
+
+    // maybe implement multiple photos as a slideshow later on
+
     return(
-        <div class="window" style="width: 300px"> 
+
+        <Rnd>
+            <div class="window"> 
             <div className="title-bar photoWindow">
-            <div className="title-bar-text ">{}</div>
-                <button aria-label="Minimize"></button>
+            <div className="title-bar-text ">Anthony Le</div>
+            <div className="title-bar-controls">
+                 <button aria-label="Minimize"></button>
                 <button aria-label="Maximize"></button>
                 <button aria-label="Close"></button>
             </div>
+            </div>
+            <div className="window-body">
+                <img src={PortfolioPhoto1} className="photo"/>
+            </div>
         </div>
+        </Rnd>
+        
     );
 }
 
