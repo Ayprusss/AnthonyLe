b78367ac65045b98ecb45069519af88aebb6f809
theant741@gmail.com <theant741@gmail.com>
2024-09-20 12:53:39 -0400

    added js and css file to StartDropdown file

--- src/Components/StartDropdown/StartDropdown.js
+++ src/Components/StartDropdown/StartDropdown.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./StartDropdown.css";
-
+import "98.css";
 
 function StartDropdown() {
     <div className="start-dropdown-div">
-        <div className="start-dropdown window">
+        <div className="window">
             <div className="title-bar">
-                <div className=" title-bar-text">Windows 98: Anthony Le</div>
+                <div className=" title-bar-text"><strong>Windows 98</strong></div>
             </div>
             <div className="windows-body">
                 <p>Check out my the apps below!</p>
+            </div>
+            <div className="app-table">
+
             </div>
         </div>
     </div>
