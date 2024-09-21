6c7573466eddecfc74b8fde9655fbc1071a75aa9
theant741@gmail.com <theant741@gmail.com>
2024-09-21 14:01:51 -0400

    added minor changes to start menu

--- src/Components/StartDropdown/StartDropdown.js
+++ src/Components/StartDropdown/StartDropdown.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./StartDropdown.css";
 import "98.css";
-
+import { Rnd } from "react-rnd";
 function StartDropdown() {
-    <div className="start-dropdown-div">
-        <div className="window">
-            <div className="title-bar">
-                <div className=" title-bar-text"><strong>Windows 98</strong></div>
-            </div>
-            <div className="windows-body">
-                <p>Check out my the apps below!</p>
-            </div>
-            <div className="app-table">
+    <Rnd>
+        <div className="start-dropdown-div">
+    <div className="window">
+        <div className="title-bar">
+            <div className=" title-bar-text"><strong>Windows 98</strong></div>
+        </div>
+        <div className="windows-body">
+            
+        </div>
+        <div className="app-table">
 
-            </div>
         </div>
     </div>
+</div>
+    </Rnd>
+   
 }
 
 
