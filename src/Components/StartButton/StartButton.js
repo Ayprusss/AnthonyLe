c6bd37aed782198dce00cb64528f0fa2793924db
theant741@gmail.com <theant741@gmail.com>
2024-09-25 17:23:30 -0400

    added minor changes to startbutton

--- src/Components/StartButton/StartButton.js
+++ src/Components/StartButton/StartButton.js
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "98.css";
 import windowslogo from "./Windows_Logo_(1992-2001).svg.png";
 import "./StartButton.css";
+import StartDropdown from "../StartDropdown/StartDropdown";
+
 
 
 
 function StartButton() {
 
+    const[isStartMenuVisible, setIsStartMenuVisible] = useState(false);
 
+    const handleStartButtonClick = () => {
+        setIsStartMenuVisible(!isStartMenuVisible);
+    };
     
     return(
-
-        <div className="start-menu">
-            <div className="start-button-div">
-                        <button className="start-button windows-box-shadow" id="start-bar-button">
-                            <img src= {windowslogo} id="start-logo" alt="windows '91-01 logo"/>
-                            <p className="logo-text">Start</p>
-                        </button>
-                    </div>
-        </div>
+        <div className="start-button-div">
+                    <button className="start-button windows-box-shadow" id="start-bar-button" onClick={handleStartButtonClick}>
+                        <img src= {windowslogo} id="start-logo" alt="windows '91-01 logo"/>
+                        <p className="logo-text">Start</p>
+                    </button>
+                    {isStartMenuVisible && <StartDropdown />}
+                </div>
     );
 }
 
