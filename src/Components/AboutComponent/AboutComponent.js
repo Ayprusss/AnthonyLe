a4bee44a322c7cb359ffe96fa34b074bff454285
theant741@gmail.com <theant741@gmail.com>
2025-01-13 11:03:43 -0500

    refactor AboutComponent to use Rnd for draggable window and add close functionality

--- src/Components/AboutComponent/AboutComponent.js
+++ src/Components/AboutComponent/AboutComponent.js
@@ -1,25 +1,30 @@
 import React from "react";
 import "98.css";
 import "./AboutComponent.css";
-
-function AboutComponent() {
+import { Rnd } from "react-rnd";
+function AboutComponent( { onClose }) {
 
 
     return(
-        <div className="window">
-            <div className="title-bar">
-                <div className="title-bar-text">About Me</div>
-                <div className="title-bar-controls">
-                    <button aria-label="Minimize"></button>
-                    <button aria-label="Maximize"></button>
-                    <button aria-label="Close"></button>
+        <Rnd>
+            <div className="window">
+                <div className="title-bar">
+                    <div className="title-bar-text">About Me</div>
+                    <div className="title-bar-controls">
+                        <button aria-label="Minimize"></button>
+                        <button aria-label="Maximize"></button>
+                        <button aria-label="Close" onClick={onClose}></button>
+                    </div>
+                </div>
+                <div className="window-body">
+                    <p>Hi there! My name is Anthony. I am a passionate web developer with a keen interest in creating interactive and user-friendly web applications. 
+                        I have experience in various web technologies including React, JavaScript, HTML, and CSS.
+                         In my free time, I enjoy learning new programming languages and exploring the latest trends in web development.</p>
                 </div>
             </div>
-            <div class="window-body">
-                
-            </div>
-        </div>
-
-
+        </Rnd>
     );
 }
+
+
+export default AboutComponent;
