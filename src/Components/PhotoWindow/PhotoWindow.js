c01990871b5e7b774d77fd630f6eb495fcdf06b4
theant741@gmail.com <theant741@gmail.com>
2025-01-13 11:03:19 -0500

    no changes

--- src/Components/PhotoWindow/PhotoWindow.js
+++ src/Components/PhotoWindow/PhotoWindow.js
@@ -2,11 +2,11 @@ import React from "react";
 import "98.css";
 import "./PhotoWindow.css";
 
-import "98.css"
 import PortfolioPhoto1 from "./portfolioPhoto.jpg"
 
 import { Rnd } from "react-rnd";
-function photoWindow() {
+
+function photoWindow( { onClose }) {
 
 
     // maybe implement multiple photos as a slideshow later on
@@ -18,9 +18,7 @@ function photoWindow() {
             <div className="title-bar photoWindow">
             <div className="title-bar-text ">Anthony Le</div>
             <div className="title-bar-controls">
-                 <button aria-label="Minimize"></button>
-                <button aria-label="Maximize"></button>
-                <button aria-label="Close"></button>
+                <button aria-label="Close" onClick={onClose}></button>
             </div>
             </div>
             <div className="window-body">
