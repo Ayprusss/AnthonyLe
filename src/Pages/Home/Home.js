6a420fc0196d0f5a560fa19e25ed28fe2292bfcb
theant741@gmail.com <theant741@gmail.com>
2025-01-13 10:57:01 -0500

    added clippyHelper and home container for separation

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,17 +4,22 @@ import Taskbar from "../../Components/TaskBar/Taskbar.js";
 import PhotoWindow from "../../Components/PhotoWindow/PhotoWindow.js";
 import TextWindow from "../../Components/TextWindow/TextWindow.js";
 import ResumeWindow from "../../Components/ResumeWindow/ResumeWindow.js";
+import StartDropdown from "../../Components/StartDropdown/StartDropdown.js";
+import ClippyHelper from "../../Components/ClippyHelper/ClippyHelper.js";
 
 function Home() {
     return(
         <div>
             <div className="home">
-                <div className="right"></div>
-                <div className="center"></div>
-                <div className="left"></div>
-                <PhotoWindow />
+                <div className="home-container">
+
+                    <div className="home-left">
+                    </div>
+                    <div className="home-right">
+                    <ClippyHelper />
+                    </div>
+                </div>
                 <TextWindow />
-                <ResumeWindow />
                 <Taskbar />
             </div>
         </div>
