155325cf95470e74c8f5e26bd07de05defc69670
theant741@gmail.com <theant741@gmail.com>
2025-01-13 10:58:28 -0500

    altered StartDropdown structure for explicit event handling

--- src/Components/StartDropdown/StartDropdown.js
+++ src/Components/StartDropdown/StartDropdown.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import "./StartDropdown.css";
 import "98.css";
 import photo1 from "./address_book_copy.png";
@@ -7,39 +7,71 @@ import photo3 from "./netshow-2.png";
 import photo4 from "./kodak_imaging-0.png";
 import photo5 from "./message_file-0.png";
 
-function StartDropdown() {
-    return(
+function StartDropdown({
+    setPhotoVisible,
+    setAboutVisible,
+    setResumeVisible,
+    setExperienceVisible,
+    setContactVisible
+}) {
+
+
+    const handleResumeClick = () => {
+        console.log('Resume click triggered');
+        setResumeVisible(true);
+    }
+
+    const handleAboutClick = () => {
+        console.log('About click triggered');
+        setAboutVisible(true);
+    };
+
+    const handleContactClick = () => {
+        console.log('Contact click triggered');
+        setContactVisible(true);
+    };
+
+    const handlePhotoClick = () => {
+        console.log('Photo click triggered');
+        setPhotoVisible(true);
+    };
+
+    const handleExperienceClick = () => {
+        console.log('Experience click triggered');
+        setExperienceVisible(true);
+    };
+    
+    return (
         <div className="window start-dropdown-div">
             <div className="title-bar">
-                <div className=" title-bar-text"><strong>Windows 98</strong></div>
+                <div className="title-bar-text"><strong>Windows 98</strong></div>
             </div>
-            <div className="windows-body">    
+            <div className="windows-body">
                 <ul className="tree-view icon-list">
-                    <li className="icon-item">
+                    <li className="icon-item" onClick={handlePhotoClick}>
                         <img alt="userPhoto" src={photo4} id="icon-photo"></img>
-                        <label for="icon-item" className="icon-text">My Photo</label>
+                        <label htmlFor="icon-item" className="icon-text">My Photo</label>
                     </li>
-                    <li className="icon-item">
+                    <li className="icon-item" onClick={handleAboutClick}>
                         <img alt="userPhoto" src={photo2} id="icon-photo"></img>
-                        <label for="icon-item" className="icon-text">About</label>
+                        <label htmlFor="icon-item" className="icon-text">About</label>
                     </li>
-                    <li className="icon-item">
+                    <li className="icon-item" onClick={handleResumeClick}>
                         <img alt="userPhoto" src={photo5} id="icon-photo"></img>
-                        <label for="icon-item" className="icon-text">My Photo</label>
+                        <label htmlFor="icon-item" className="icon-text">My Resume</label>
                     </li>
-                    <li className="icon-item">
+                    <li className="icon-item" onClick={handleExperienceClick}>
                         <img alt="phot1o" src={photo1} id="icon-photo"></img>
-                        <label for="icon-item" className="icon-text">Experience</label>
+                        <label htmlFor="icon-item" className="icon-text">Experience</label>
                     </li>
-                    <li className="icon-item">
+                    <li className="icon-item" onClick={handleContactClick}>
                         <img alt="contactPhoto" id="icon-photo" src={photo3}></img>
-                        <label for="icon-item" className="icon-text">Contact Me</label>
+                        <label htmlFor="icon-item" className="icon-text">Contact Me</label>
                     </li>
                 </ul>
             </div>
         </div>
     );
 }
-
 
 export default StartDropdown;
