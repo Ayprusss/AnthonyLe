a1aec8f56f411a8c9e0110387fc19f919fcda175
ayprusss <theant741@gmail.com>
2025-04-28 17:43:42 -0400

    added more detail to AboutComponent.

--- src/Components/AboutComponent/AboutComponent.js
+++ src/Components/AboutComponent/AboutComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import "98.css";
 import "./AboutComponent.css";
 import { Rnd } from "react-rnd";
+import resume from "../ResumeWindow/resume.pdf";
 function AboutComponent( { onClose }) {
 
 
@@ -17,9 +18,15 @@ function AboutComponent( { onClose }) {
                     </div>
                 </div>
                 <div className="window-body">
-                    <p>Hi there! My name is XYZ. I am a passionate Software Engineer with a keen interest in creating . 
-                        I have experience in various web technologies including React, JavaScript, HTML, and CSS.
-                         In my free time, I enjoy learning new programming languages and exploring the latest trends in web development.</p>
+                    <p>Hi there!<b> My name is Anthony Le.</b></p>
+                    <p>I am currently a third-year student studying Computer Science at the Univeristy of Ottawa. I am an aspiring <b>Software Engineer</b> with vast experience around <b>web development. </b>
+                      I also have experience in <b>Mobile App Development, Data Science and some Machine Learning.</b></p>
+                    
+                    <p>My main goal is to become a full-stack developer, and I am currently gaining experience with <b>back-end development</b> and am looking for internships or co-op opportunities to gain more experience in the field.</p>
+                    <br></br>
+                    <p>For the time being, my website is still in development. Please feel free to check out <a href={resume} rel="noreferrer" target="_blank">my resume</a> for my experience and information.</p>
+                    <br></br>
+                    <p>Want to contact me? <b>Click Here!</b></p>
                 </div>
             </div>
         </Rnd>
