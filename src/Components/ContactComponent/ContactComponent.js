8530cae7b9413145d42592c635b9eb435872d80e
ayprusss <theant741@gmail.com>
2025-04-28 17:44:11 -0400

    added contactCompnonent

---
+++ src/Components/ContactComponent/ContactComponent.js
@@ -0,0 +1,62 @@
+import React, {useRef} from 'react';
+import "98.css";
+import "./ContactComponent.css";
+import { Rnd } from "react-rnd";
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
+function ContactComponent({ onClose }) {
+
+
+    const [notificationVisible, setNotificationVisible] = useState(false);
+
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('ayprusss_email_service', 'template_30qbmwr', form.current, 'ajgDW7hgrON568ajG')
+        .then((result) => {
+            console.log('Email sent successfully: ', result.text);
+            setTimeout(() => {
+                setNotificationVisible(true);
+                onClose(); // Close the window after 3 seconds
+            }, 1500); // 3000 milliseconds = 3 seconds
+        }, (error) => {
+            console.log('Failed to send email: ', error.text);
+        });
+    };
+
+
+    return(
+        <Rnd
+             dragHandleClassName='title-bar'>
+            <div className="window contact-window">
+                <div className="title-bar">Contact Me
+                    <div className="title-bar-controls">
+                        <button aria-label="Minimize"></button>
+                        <button aria-label="Maximize"></button>
+                        <button aria-label="Close" onClick={onClose}></button>
+                    </div>
+                </div>
+                <div className="window-body contact-body">
+                    <p>Looking to contact me? Please fill out the form and I will get back to you ASAP.</p>
+                    <form ref={form} onSubmit={sendEmail} className="contact-form">
+                        <fieldset>
+                        <div class="field-row-stacked">
+                            <label className="contact-label" for="contact-name">Name:</label>
+                            <input id="contact-name" type="text" name="name" required/>
+                            <label className="contact-label" for="contact-email">Email:</label>
+                            <input id="contact-email" type="email" name="email" required />
+                            <label className="contact-label" for="contact-message">Message:</label>
+                            <textarea id="contact-message" name="message" rows="10" required />
+
+                            <button type="submit" class="default">Send</button>
+                        </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </Rnd>
+    );
+}
+
+export default ContactComponent;
