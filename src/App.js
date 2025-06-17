3d2f6aaeea1864a7315e574b724f2047a8a3d915
theant741@gmail.com <theant741@gmail.com>
2025-06-17 13:38:28 -0400

    adding some more html structure to app component

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import Home from "./Pages/Home/Home.js";
 
 function App() {
   return (
-    <body>
-      <head>
-        <link href="os-gui/layout.css" rel="stylesheet" type="text/css" />
-        <link href="os-gui/windows-98.css" rel="stylesheet" type="text/css" />
-        <link href="os-gui/windows-default.css" rel="stylesheet" type="text/css" />
-      </head>
-      <script src="os-gui/MenuBar.js"></script>   
-      <script src="lib/jquery.js"></script>
-      <script src="os-gui/$Window.js"></script>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
-    </body>
+    <html lang="en">
+      <meta charset="utf-8"/>
+        <title>Anthony Le's Personal Website</title>
+        <head>
+          <link href="os-gui/layout.css" rel="stylesheet" type="text/css" />
+          <link href="os-gui/windows-98.css" rel="stylesheet" type="text/css" />
+          <link href="os-gui/windows-default.css" rel="stylesheet" type="text/css" />
+        </head>
+      <body>
+        <script src="os-gui/MenuBar.js"></script>   
+        <script src="lib/jquery.js"></script>
+        <script src="os-gui/$Window.js"></script>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+      </body>
+    </html>
     
   );
 }
