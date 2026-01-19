0262b8d589b03dc8e89e36e9f761ecf7cd1cbb90
Anthony Le <theant741@gmail.com>
2026-01-19 18:59:17 -0500

    changed to new home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./Pages/Home/Home.js";
+import Home from "./Pages/HomeV2/Home";
 
 
 function App() {
