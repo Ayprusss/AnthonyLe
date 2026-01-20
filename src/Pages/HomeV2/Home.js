a53c9926c7560d0a00af821950c0a5d38be8c9d1
Anthony Le <theant741@gmail.com>
2026-01-19 19:00:11 -0500

    deleted previous implementations

---
+++ src/Pages/HomeV2/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return(
+        <main className="home-page">
+            Hi!
+
+        </main>
+    );
+}
+
+export default Home;
