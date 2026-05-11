2daa59ffd6b8e262aa91e18afa2daf41793ba115
Anthony Le <theant741@gmail.com>
2026-05-11 19:37:16 -0400

    changed base route for App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Desktop from "./Windows98/Desktop/Desktop";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Desktop />} />
+        <Route path="/site" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
